d8aab2d7599ebba307acecfd710e00f6bb5bdd4c
ashis <bantawa.ashis@gmail.com>
2016-07-04 14:19:40 +0545

    Remove extra space, add semicolon.

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,9 @@
 var HtmlWebpackPlugin = require('html-webpack-plugin');
-var HtmlWebpackPluginConfig = new  HtmlWebpackPlugin({
+var HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
 	template: __dirname + '/app/index.html',
 	filename: 'index.html',
 	inject: 'body'
-})
+});
 
 module.exports = {
 	entry: [
