bb6b3e611eaffd24855e9e0b03a56ba252ae189f
a5his <bantawa.ashis@gmail.com>
2016-07-06 21:56:08 +0545

    Fix function name.

--- app/components/prompt.js
+++ app/components/prompt.js
@@ -15,12 +15,12 @@ var Prompt = React.createClass({
 			<div className = "jumbotron col-sm-6 col-sm-offset-3 text-center" style={transparentBg}>
 				<h1>{this.props.header}</h1>
 				<div className = "col-sm-12">
-					<form onSubmit={this.handleSubmitUser}>
+					<form onSubmit={this.props.onSubmitUser}>
 						<div className="form-group">
 							<input
 								className="form-control"
 								placeholder="Github username"
-								onChange={this.handleUpdateUser}
+								onChange={this.props.onUpdateUser}
 								value={this.props.username}
 								type="text"/>
 						</div>
